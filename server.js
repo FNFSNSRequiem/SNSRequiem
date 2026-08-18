c9eb6abc70cfe98306dc35306504efa8b0de908d
FNF: SNS Requiem <snsrequiem@gmail.com>
2026-08-18 06:37:39 +0000

    TEST #3

--- server.js
+++ server.js
@@ -1,43 +1,6 @@
-const express = require("express");
-const path = require("path");
-
-const app = express();
+const HOST = "127.0.0.1";
 const PORT = process.env.PORT || 3000;
 
-// Serve everything inside /public
-app.use(express.static(path.join(__dirname, "public")));
-
-// Homepage
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-// 404
-app.use((req, res) => {
-    res.status(404).send(`
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>404 - SNS REQUIEM</title>
-            <style>
-                body {
-                    background: #000;
-                    color: #fff;
-                    font-family: monospace;
-                    text-align: center;
-                    padding-top: 20vh;
-                }
-            </style>
-        </head>
-        <body>
-            <h1>404</h1>
-            <p>PAGE NOT FOUND.</p>
-            <a href="/">RETURN</a>
-        </body>
-        </html>
-    `);
-});
-
-app.listen(PORT, () => {
-    console.log(`SNS REQUIEM is running at http://localhost:${PORT}`);
+app.listen(PORT, HOST, () => {
+    console.log(`SNS: REQUIEM running at http://${HOST}:${PORT}`);
 });
