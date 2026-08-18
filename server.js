37d1699094509669258300965dfc96641e62f8f0
FNF: SNS Requiem <snsrequiem@gmail.com>
2026-08-18 06:40:48 +0000

    TEST #3

--- server.js
+++ server.js
@@ -1,6 +1,55 @@
-const HOST = "127.0.0.1";
+const express = require("express");
+const path = require("path");
+
+const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, HOST, () => {
-    console.log(`SNS: REQUIEM running at http://${HOST}:${PORT}`);
+// Serve everything inside /public
+app.use(express.static(path.join(__dirname, "public")));
+
+// Homepage
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+// 404
+app.use((req, res) => {
+    res.status(404).send(`
+        <!DOCTYPE html>
+        <html>
+        <head>
+            <meta charset="UTF-8">
+            <title>404 - SNS REQUIEM</title>
+            <style>
+                body {
+                    background: #e5dfc9;
+                    color: #171717;
+                    font-family: Georgia, "Times New Roman", serif;
+                    text-align: center;
+                    padding-top: 20vh;
+                }
+
+                h1 {
+                    font-family: Georgia, serif;
+                    font-size: 60px;
+                }
+
+                a {
+                    color: #171717;
+                }
+            </style>
+        </head>
+        <body>
+            <h1>404</h1>
+            <p>THE PICTURE COULD NOT BE FOUND.</p>
+            <a href="/">RETURN TO THE THEATER</a>
+        </body>
+        </html>
+    `);
+});
+
+// Only expose Node locally.
+// Nginx will handle public connections.
+app.listen(PORT, "127.0.0.1", () => {
+    console.log(`SNS REQUIEM is running on http://127.0.0.1:${PORT}`);
 });
